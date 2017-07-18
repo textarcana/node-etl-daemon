e6254ea0875b5f5c265f395e5a421bc050206a51
Noah Sussman <noah@onemorebug.com>
2017-07-18 18:06:15 -0400

    simplify retreiver functions
    add eslint rc file
    
    schedule a watcher that prints the contents of the data store

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
-const subscriber = require('./lib/subscriber-daemon.js');
+const logger = require('winston');
+const request = require('request-promise');
 
-const placeholder = require('./lib/placeholder-api.js');
+const subscriber = require('./lib/subscriber-daemon.js');
+const scheduler = require('./lib/scheduler.js');
 
 const data = {};
 
-subscriber(placeholder, 'TEMP.json', data)
+const retriever = (options) => {
+  logger.info('Making remote API Request made to: ', options.uri);
+
+  options.json = true;
+
+  return request(options)
+    .catch((e) => {
+      throw Error(e);
+    });
+};
+
+const placeholder = () => {
+  const options = {};
+
+  options.uri = 'https://jsonplaceholder.typicode.com/posts';
+
+  return retriever(options);
+};
+
+const wikipedia = () => {
+  const options = {};
+
+  options.uri = 'https://en.wikipedia.org/w/api.php' +
+    '?format=json&action=query&titles=Software_testing&prop=contributors';
+
+  return retriever(options);
+};
+
+subscriber(placeholder, 'lorem.json', data, 'placeholder')
+  .start();
+
+subscriber(wikipedia, 'wikipedia.json', data, 'wiki')
+  .start();
+
+scheduler(() => {
+  logger.info('Wiki data: ', data.wiki);
+})
   .start();
